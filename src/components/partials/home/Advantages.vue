58befd091d7d5789049e0f65ab53aedad577458e
andreattard <andre.attard.a100189@mcast.edu.mt>
2021-07-17 18:38:29 +0200

    Advatages section in home page both mobile and desktop

--- src/components/partials/home/Advantages.vue
+++ src/components/partials/home/Advantages.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="container" v-if="!isHomeLoading">
-        <h1>ADVANTAGES SECTION</h1>
-        <!-- <p>{{ setAdvantagesSectionData }}</p> -->
-    </div>
+    <section class="advantages-section-home" v-if="!isHomeLoading" v-bind:style="{ backgroundImage: 'url(' + setAdvantagesSectionData.background.url + ')' }">
+        <div class="container">
+            <div class="column" v-for="item in setAdvantagesSectionData.advantages" :key="item.advantage.title">
+                <img :src="item.advantage.icon.url" :alt="item.advantage.icon.alt" />
+                <h4 v-html="item.advantage.title"></h4>
+                <div v-html="item.advantage.text"></div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
