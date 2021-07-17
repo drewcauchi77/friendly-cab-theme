ccedf66be10d306f7349d4e7b51a1365071065fb
andreattard <andre.attard.a100189@mcast.edu.mt>
2021-07-17 13:25:45 +0200

    How it works section in Homepage (mobile and desktop)

--- src/components/partials/home/HowItWorks.vue
+++ src/components/partials/home/HowItWorks.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="container" v-if="!isHomeLoading">
-        <h1>HOW IT WORKS SECTION</h1>
-        <!-- <p>{{ setHowItWorksSectionData }}</p> -->
+    <div class="container how-it-works" v-if="!isHomeLoading">
+        <div class="section-title text-center lower-below-text-border" v-html="setHowItWorksSectionData.title"></div>
+        <div class="three-col-section">
+            <div class="column" v-for="item in setHowItWorksSectionData.details" :key="item.item.title">
+                <img :src="item.item.icon.url" :alt="item.item.icon.alt"/>
+                <h3>{{item.item.title}}</h3>
+                <p class="normal-text" v-html="item.item.text"></p>
+            </div>
+        </div>
     </div>
 </template>
 
