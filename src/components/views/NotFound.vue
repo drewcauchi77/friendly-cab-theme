112492c65d5c3b128784ee90bf57a69b1c48b0a6
Andrew Cauchi <cauchi1020@gmail.com>
2021-07-19 18:46:41 +0200

    Created and styled the error page

--- src/components/views/NotFound.vue
+++ src/components/views/NotFound.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>404</h1>
+    <div class="error-content" v-if="!isErrorLoading">
+        <div class="page-content container">
+            <h1>Oops! Page not found</h1>
+            <img src="/wp-content/themes/zuus-vuejs-wp-theme-boilerplate/dist/assets/404.png">
+            <router-link to="/" class="button-blue">Go to home</router-link>
+        </div>
+    </div>
+
+    <Loading v-else />
 </template>
 
 <script>
+import Loading from '../partials/global/Loading'
+
 export default {
-    name: 'NotFound'
+    name: 'NotFound',
+    components: {
+        Loading
+    },
+    data() {
+        return {
+            isErrorLoading: true
+        }
+    },
+    mounted() {
+        this.isErrorLoading = false
+    }
 }
 </script>
