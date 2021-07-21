41f196f2d4225ac13567f4cb2165f2b5b13a757e
andreattard <andre.attard.a100189@mcast.edu.mt>
2021-07-21 22:50:28 +0200

    testimonials section within home page, major part of functionality and styling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueMeta from 'vue-meta'
+import VueSlickCarousel from 'vue-slick-carousel'
 
 import App from './App.vue'
 import router from './router'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueSlickCarousel)
 Vue.use(VueMeta)
 
 new Vue({
--- src/components/partials/home/Testimonials.vue
+++ src/components/partials/home/Testimonials.vue
@@ -1,15 +1,70 @@
 <template>
     <div class="container" v-if="!isHomeLoading">
-        <h1>TESTIMONIALS SECTION</h1>
-        <!-- <p>{{ setTestimonialsSectionData }}</p> -->
+        <div class="testimonials-section">
+            <div class="section-title text-center lower-below-text-border" v-html="setTestimonialsSectionData.title"></div>
+            <VueSlickCarousel v-bind="settings">
+               <div class="card" v-for="item in setTestimonialsSectionData.testimonials" :key="item.testimonial.ID">
+                    <div class="inner-card">
+                        <p class="normal-text feedback" v-html="item.testimonial.acf.content.feedback"></p>
+                        <div class="user-section">
+                            <img :src="item.testimonial.acf.content.avatar.url" :alt="item.testimonial.acf.content.avatar.url"/>
+                            <h4>{{item.testimonial.post_title}}</h4>
+                            <p class="normal-text">{{item.testimonial.acf.content.title}}</p>
+                        </div>
+                    </div>
+               </div>
+            </VueSlickCarousel>
+        </div>
     </div>
 </template>
 
 <script>
 import { setHomeContentMixin } from '../../../mixins'
+import VueSlickCarousel from 'vue-slick-carousel'
 
 export default {
     name: 'Testimonials',
+    components: {
+        VueSlickCarousel
+    },
+    data() {
+        return {
+            settings:{
+                infinite: false,
+                slidesToShow: 3,
+                speed: 500,
+                swipe: true,
+                slidesToScroll: 1,
+                responsive: [
+                    {
+                    breakpoint: 1600,
+                        settings: {
+                            slidesToShow: 3
+                        }
+                    },
+                    {
+                    breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                    breakpoint: 993,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                    breakpoint: 650,
+                        settings: {
+                            slidesToShow: 1,
+                        }
+                    }
+                ]
+               
+            },    
+        }
+    },
     mixins: [setHomeContentMixin]
 }
 </script>
