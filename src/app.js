49d894230b738912b7f62600d394cd9e5f64040a
Andrew Cauchi <cauchi1020@gmail.com>
2021-07-25 14:45:38 +0200

    Created and styled footer, installed font-awesome, fixed minor bug issues

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import VueSlickCarousel from 'vue-slick-carousel'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMapMarkerAlt, faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App.vue'
 import router from './router'
 import 'vue-slick-carousel/dist/vue-slick-carousel.css'
@@ -10,6 +15,16 @@ Vue.config.productionTip = false
 
 Vue.use(VueSlickCarousel)
 Vue.use(VueMeta)
+
+library.add(faMapMarkerAlt)
+library.add(faEnvelope)
+library.add(faPhoneAlt)
+library.add(faFacebook)
+library.add(faTwitter)
+library.add(faInstagram)
+library.add(faLinkedin)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
     el: '#app',
